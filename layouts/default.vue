b4ad0371b28ff5d03108841c874e0e7ab421de32
ParinyaWante <parinyaw@8interactive.co.th>
2023-11-05 17:43:23 +0700

    Global State Management

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,14 @@
-<script lang="ts" setup></script>
+<script setup>
+const isLoggedIn = useIsLoggedIn();
+</script>
 
 <template>
   <nav style="display: flex; gap: 10px">
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/Movies">Movies</NuxtLink>
     <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+    <NuxtLink v-if="!isLoggedIn" to="/login">Login</NuxtLink>
+    <a href="#" v-else @click.prevent="isLoggedIn = false">Logout</a>
   </nav>
   <slot></slot>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,18 +1,29 @@
 <script lang="ts" setup>
+const isLoggedIn = useIsLoggedIn();
+
 // definePageMeta() use for specified the layout that you want to use
 definePageMeta({
   layout: "plain",
 });
+
+const login = () => {
+  isLoggedIn.value = true;
+  useRouter().push("/");
+};
 </script>
 
 <template>
-  <h1>Login</h1>
-  <label>
-    Usename
-    <input type="text" />
-  </label>
-  <label>
-    Password
-    <input type="password" />
-  </label>
+  <form @submit.prevent="login">
+    <h1>Login</h1>
+    <label>
+      Usename
+      <input type="text" />
+    </label>
+    <label>
+      Password
+      <input type="password" />
+    </label>
+
+    <button>Login</button>
+  </form>
 </template>
